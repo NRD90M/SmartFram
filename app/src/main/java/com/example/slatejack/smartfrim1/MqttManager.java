3b305dd43a516d7f40f770ca4877cdcb16e37e40
Slate <1815846446@qq.com>
2019-06-11 08:45:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/slatejack/smartfrim1/MqttManager.java
@@ -0,0 +1,141 @@
+package com.example.slatejack.smartfrim1;
+
+import android.app.Activity;
+import android.util.Log;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MqttManager {
+    public static final String TAG = MqttManager.class.getSimpleName();
+
+    private String host;
+    private String username;
+    private String password;
+    private String clientId;
+    //创建用于连接mqtt服务器的相关内容，分别是服务器的地址
+//还有就是服务器的相关密码账户，还有客户端的ID
+    private MqttClient client;
+    //创建mqttClient对象，以及连接的对象
+    private MqttConnectOptions connectOptions;
+
+    /**
+     * 三个参数的构造方法
+     * 以及自动获取客户端的ID
+     * 这就是上面的三个参数，还有一个是当时准备传入主UI线程的，后来就没用到
+     *  @param host
+     *
+     */
+    public MqttManager(String host,Activity activity){
+        this.host=host;
+        this.clientId= MqttClient.generateClientId();
+    }
+
+
+    /**
+     * 连接操作
+     * 首先创建一个客户端对象，同时把地址，id传进去
+     * 其次再把账户密码穿进去
+     * 输入一个callback实例
+     * 开始连接服务器
+     */
+    public void connect(){
+        try{
+            client = new MqttClient(host,clientId,new MemoryPersistence());
+            connectOptions = new MqttConnectOptions();
+            client.setCallback(mqttCallback);
+            client.connect(connectOptions);
+
+        }catch (MqttException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取函数
+     * 下面两个参数主要是一个是消息对列的名称，一个是服务质量
+     * 然后把这两个对象装程数组类型
+     * 然后通过调用客户端的函数subscribe来读取消息
+     *
+     * @param topic
+     * @param qos
+     */
+
+    public void subscribe(String topic, int qos) {
+        if (client != null) {
+            int[] Qos = {qos};
+            String[] topic1 = {topic};
+            try {
+                client.subscribe( topic1, Qos );
+                Log.d( TAG, "订阅topic : " + topic );
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 此为向服务器发布数据的操作封装函数
+     * 在这里的四个参数的意思分别是
+     *
+     * @param topic      消息队列
+     * @param msg        向服务器发送到消息
+     * @param isRetained 是否保留，一般false
+     * @param qos        服务质量
+     *                   新建一个MqttMessa对象然后分别通过对应的set函数将
+     *                   这四个参数穿进去然后调用客户端里面的publish函数进行
+     *                   获取服务器里面的信息
+     *                   并打印日志
+     */
+
+    public void publish(String topic, String msg, boolean isRetained, int qos) {
+        try {
+            if (client != null) {
+                MqttMessage message = new MqttMessage();
+                message.setQos( qos );
+                message.setRetained( isRetained );
+                message.setPayload( msg.getBytes() );
+                client.publish( topic, message );
+                Log.i( TAG, "publish: zzzzzzzzz" );
+            }
+        } catch (MqttPersistenceException e) {
+            e.printStackTrace();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    String payload;
+    private MqttCallback mqttCallback = new MqttCallback() {
+        @Override
+        public void connectionLost(Throwable cause) {
+            Log.i( TAG, "connection lost" );
+        }
+
+        @Override
+        public void messageArrived(String topic, MqttMessage message) {
+            Log.i( TAG, "received topic : " + topic );
+            payload = new String( message.getPayload() );
+            //捕获从服务器端发送过来的信息通过下面的a（）函数传出去
+            Log.i( TAG, "received msg : " + payload );
+
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken token) {
+            Log.i( TAG, "deliveryComplete" );
+        }
+    };
+
+    public String apply() {
+        //别的类读取信息调用此函数即可
+        return payload;
+    }
+
+}
---
+++ app/src/main/java/com/example/slatejack/smartfrim1/nongchang.java
@@ -0,0 +1,116 @@
+package com.example.slatejack.smartfrim1;
+
+import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class nongchang extends AppCompatActivity {
+    private Map<String,String> map;
+    private List<Map<String,String>> list;
+    private TextView tvcity;
+    private TextView tvweather;
+    private TextView tvtemp;
+    private TextView tvwind;
+    private TextView tvpm;
+    private ImageView tvicon;
+    private  MqttManager mqttManager ;
+    private BaseAdapter baseAdapter;
+    private TextView chuangan1_name1;
+    private TextView chuangan1_name2;
+    private TextView chuangan2_name1;
+    private TextView chuangan2_name2;
+    private TextView chuangan3_name1;
+    private TextView chuangan3_name2;
+    private TextView chuangan4_name1;
+    private TextView chuangan4_name2;
+    private TextView chuangan5_name1;
+    private TextView chuangan5_name2;
+    private TextView chuangan6_name1;
+    private TextView chuangan6_name2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_nongchang );
+        //初始化控件
+        initView();
+        //解析xml文件
+        InputStream inputStream =getResources().openRawResource( R.raw.weather );
+        try {
+            List<weatherinfo> weatherinfos=weatherservice.getInfoFromXml( inputStream );
+            list=new ArrayList<Map<String,String>>();
+            for (weatherinfo info:weatherinfos){
+                map=new HashMap<>();
+                map.put( "temp",info.getTemp() );
+                map.put( "weather",info.getWeather() );
+                map.put( "wind",info.getWind() );
+                map.put( "pm",info.getPm() );
+                map.put( "name",info.getName() );
+                list.add( map );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //连接mqtt服务器
+        mqttManager=new MqttManager("tcp://118.24.19.135;1883",nongchang.this);
+        mqttManager.connect();
+        Toast.makeText(getApplicationContext(),"连接成功", Toast.LENGTH_SHORT).show();
+        //显示天气控件到文本框中
+        getMap(1,R.drawable.sun);
+        getmessage();
+    }
+
+    //天气信息
+    private void getMap(int number, int iconnumber) {
+        Map<String,String> citymap=list.get( number-1 );
+        String weather=citymap.get( "weather" );
+        String name=citymap.get( "name" );
+        String pm=citymap.get( "pm" );
+        String wind=citymap.get( "wind" );
+        String temp=citymap.get("temp");
+        tvcity.setText( name );
+        tvweather.setText( weather );
+        tvtemp.setText( temp );
+        tvwind.setText( wind );
+        tvpm.setText( pm );
+        tvicon.setImageResource( iconnumber );
+
+    }
+
+    private void getmessage(){
+        mqttManager=new MqttManager("tcp://118.24.19.135;1883",nongchang.this);
+        String message1;
+        message1= mqttManager.apply();
+        chuangan1_name2.setText( message1 );
+    }
+
+    private void initView() {
+      tvcity = findViewById( R.id.city );
+      tvweather=findViewById( R.id.weather );
+      tvtemp=findViewById( R.id.temp );
+      tvwind=findViewById( R.id.wind );
+      tvpm=findViewById( R.id.pm );
+      tvicon=findViewById( R.id.imageView2 );
+      chuangan1_name1=findViewById( R.id.chuangan1_name1 );
+      chuangan1_name2=findViewById( R.id.chuangan1_name2 );
+      chuangan2_name1=findViewById( R.id.chuangan2_name1 );
+      chuangan2_name2=findViewById( R.id.chuangan2_name2 );
+      chuangan3_name1=findViewById( R.id.chuangan3_name1 );
+      chuangan3_name2=findViewById( R.id.chuangan3_name2 );
+      chuangan4_name1=findViewById( R.id.chuangan4_name1 );
+      chuangan4_name2=findViewById( R.id.chuangan4_name2 );
+      chuangan5_name1=findViewById( R.id.chuangan5_name1 );
+      chuangan5_name2=findViewById( R.id.chuangan5_name2 );
+      chuangan6_name1=findViewById( R.id.chuangan6_name1 );
+      chuangan6_name2=findViewById( R.id.chuangan6_name2 );
+    }
+}
